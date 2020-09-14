75dff90d5eaa1e180cd5626acb21edffc5c5c55e
emil257 <emil.saxlund@live.se>
2020-09-14 14:08:21 +0200

    project-start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
 import './assets/css/custom.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 import AppFooter from '../src/components/app-footer'
+import Home from '../src/views/Home'
 
 function App() {
   return (
-    <div>
-      <AppFooter/>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about">
+            <div>
+              Put view here
+            </div>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+        <AppFooter/>
+      </div>
+    </Router>
   );
 }
 
