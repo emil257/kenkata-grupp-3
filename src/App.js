35d012705321dd35bcd749426d3ced5b7cb1a65a
emil257 <emil.saxlund@live.se>
2020-09-14 13:21:51 +0200

    f1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 import './assets/css/custom.css'
-import logo from '../src/assets/img/404.png'
 
 import AppFooter from '../src/components/app-footer'
 
 function App() {
   return (
     <div>
-      <img src={logo} alt=""/>
-      <p className="text-theme-color">hej</p>
       <AppFooter/>
     </div>
   );
