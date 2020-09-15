6dfbd920e859c42fb809243be9961ec0861d98dc
Danial Naseri <naseri.daniel24@gmail.com>
2020-09-15 11:20:25 +0200

    done app-customer-banner, app-flash-sale-banner, app-shipping

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 import Banner from '../../components/app-banner/index'
+import Shipping from '../../components/app-shipping/index'
+import CustomerBanner from '../../components/app-customer-banner'
+import FlashSaleBanner from '../../components/app-flash-sale-banner'
 
 
 export default function index() {
   return (
     <div>
-    <Banner></Banner>
+    <Banner />
+    <Shipping />
+    <FlashSaleBanner />
+    <CustomerBanner />
     </div>
   )
 }
