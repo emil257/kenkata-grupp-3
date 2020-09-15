2c8b6d8f1a325c231b42cec69ddfd3b1426090c8
Jonas Näsman <jonas.nasman87@gmail.com>
2020-09-15 11:24:15 +0200

    shop category grid

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
-import Banner from '../../components/app-banner/index'
-
+import Banner from "../../components/app-banner/index";
+import ShopCategory from "../../components/app-shop-category/index";
 
 export default function index() {
   return (
     <div>
-    <Banner></Banner>
+      <Banner></Banner>
+      <ShopCategory></ShopCategory>
     </div>
-  )
+  );
 }
