4973a69ac7ef86f13de59b10d84a2cb6d7114a25
frontendmikeholmstrom <mikael_holmstrom@hotmail.com>
2020-09-15 11:35:08 +0200

    section headers

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
 import Banner from '../../components/app-banner/index'
+import ShopByCatHeader from '../../components/app-section-headers/shop-by-category/index.js'
+import NewArrivalsHeader from '../../components/app-section-headers/new-arrivals'
+import FeaturedProductsHeader from '../../components/app-section-headers/featured-products/index'
+import TopSellersHeader from '../../components/app-section-headers/top-sellers/index'
+import LatestBlogHeader from '../../components/app-section-headers/latest-blog/index'
+import YouMayAlsoLikeHeader from '../../components/app-section-headers/you-may-also-like/index'
+import RelatedProductsHeader from '../../components/app-section-headers/related-products/index'
 
 
 export default function index() {
   return (
     <div>
+      
     <Banner></Banner>
+    <ShopByCatHeader></ShopByCatHeader>
+    <NewArrivalsHeader></NewArrivalsHeader>
+    <FeaturedProductsHeader></FeaturedProductsHeader>
+    <TopSellersHeader></TopSellersHeader>
+    <LatestBlogHeader></LatestBlogHeader>
+    <YouMayAlsoLikeHeader></YouMayAlsoLikeHeader>
+    <RelatedProductsHeader></RelatedProductsHeader>
+
     </div>
   )
 }
