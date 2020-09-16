a715e54c417d2ac57a82c04ec44b2bc87f4a4d57
emil257 <emil.saxlund@live.se>
2020-09-16 09:19:39 +0200

    m

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import './assets/css/custom.css'
 
 import AppFooter from '../src/components/app-footer'
+import AppCarousel from '../src/components/app-carousel'
 
 function App() {
   return (
     <div>
       <AppFooter/>
+      <AppCarousel/>
     </div>
   );
 }
