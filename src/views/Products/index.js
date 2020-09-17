2059f9d08897acf7b0d6e971965374e87ec98609
Ali Adrian <alinasrat95@gmail.com>
2020-09-17 11:10:33 +0200

    app-product-reviews

--- src/views/Products/index.js
+++ src/views/Products/index.js
@@ -3,6 +3,7 @@ import Shipping from '../../components/app-shipping/index'
 import Subscribe from '../../components/app-subscribe/index'
 import Brands from '../../components/app-brands/index'
 import NavigationProductsView from '../../components/app-navigation/app-navigation-products-view/index'
+import AppProductReview from '../../components/app-product-reviews/index'
 
 
 
@@ -11,6 +12,7 @@ export default function index() {
     return (
         <div>
             <NavigationProductsView></NavigationProductsView>
+            <AppProductReview></AppProductReview>
             <Shipping></Shipping>
             <Subscribe></Subscribe>
             <Brands></Brands>
