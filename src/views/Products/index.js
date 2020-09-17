9e914d658493a2273398b3e65de65d88f36955fd
Jonas Näsman <jonas.nasman87@gmail.com>
2020-09-17 11:08:59 +0200

    product desc

--- src/views/Products/index.js
+++ src/views/Products/index.js
@@ -3,7 +3,7 @@ import Shipping from '../../components/app-shipping/index'
 import Subscribe from '../../components/app-subscribe/index'
 import Brands from '../../components/app-brands/index'
 import NavigationProductsView from '../../components/app-navigation/app-navigation-products-view/index'
-
+import ProductSection from '../../components/app-product-section/index'
 
 
 
@@ -11,6 +11,7 @@ export default function index() {
     return (
         <div>
             <NavigationProductsView></NavigationProductsView>
+            <ProductSection></ProductSection>
             <Shipping></Shipping>
             <Subscribe></Subscribe>
             <Brands></Brands>
