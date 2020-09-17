b7d56b93fddb8a18427c5d5879eb2978ba6413f6
Jonas Näsman <jonas.nasman87@gmail.com>
2020-09-17 15:53:06 +0200

    product shipping

--- src/views/Products/index.js
+++ src/views/Products/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import Shipping from '../../components/app-shipping/index'
 import Subscribe from '../../components/app-subscribe/index'
 import Brands from '../../components/app-brands/index'
 import NavigationProductsView from '../../components/app-navigation/app-navigation-products-view/index'
 import ProductSection from '../../components/app-product-section/index'
 import AppProductReview from '../../components/app-product-reviews/index'
+import Shipping from '../../components/app-shipping-dark/index'
 
 export default function index() {
     return (
         <div>
+
             <NavigationProductsView></NavigationProductsView>
             <ProductSection></ProductSection>
             <AppProductReview></AppProductReview>
