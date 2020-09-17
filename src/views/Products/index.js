1a7390f8a7bff403d26a1cc60807e2fb8ba8e136
Ali Adrian <alinasrat95@gmail.com>
2020-09-17 11:21:18 +0200

    in-order-product-page

--- src/views/Products/index.js
+++ src/views/Products/index.js
@@ -3,18 +3,15 @@ import Shipping from '../../components/app-shipping/index'
 import Subscribe from '../../components/app-subscribe/index'
 import Brands from '../../components/app-brands/index'
 import NavigationProductsView from '../../components/app-navigation/app-navigation-products-view/index'
-import AppProductReview from '../../components/app-product-reviews/index'
-
 import ProductSection from '../../components/app-product-section/index'
-
-
+import AppProductReview from '../../components/app-product-reviews/index'
 
 export default function index() {
     return (
         <div>
             <NavigationProductsView></NavigationProductsView>
-            <AppProductReview></AppProductReview>
             <ProductSection></ProductSection>
+            <AppProductReview></AppProductReview>
             <Shipping></Shipping>
             <Subscribe></Subscribe>
             <Brands></Brands>
