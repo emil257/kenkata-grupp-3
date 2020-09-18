6f56fd7b76e80dace8ba90dd2633c30cc1680170
emil257 <emil.saxlund@live.se>
2020-09-18 12:09:18 +0200

    login profile

--- src/views/Account/index.js
+++ src/views/Account/index.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { loadUser, logout } from '../../store/actions/user'
+import { logout } from '../../store/actions/user'
 
 import AppLogin from '../../components/app-account/app-login'
 import AppRegister from '../../components/app-account/app-register'
 import AppAccountProfile from '../../components/app-account/app-account-profile'
 import AppBrands from '../../components/app-brands'
+import AppCurrentPath from '../../components/app-navigation/app-current-path'
 
 
 const checkLoggedIn = (loggedIn, user) => {
@@ -35,11 +36,11 @@ export default function Account() {
 
   return (
     <div>
-    
-    <div className="container">
-        {checkLoggedIn(loggedIn, user, () => dispatch(logout()))}
+      <AppCurrentPath/>
+      <div className="container">
+          {checkLoggedIn(loggedIn, user, () => dispatch(logout()))}
+      </div>
+      <AppBrands/>
     </div>
-    <AppBrands/>
-  </div>
   )
 }
