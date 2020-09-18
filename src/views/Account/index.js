5f1ac1ce3baeae8b97ed2967dcca82bda22c038f
emil257 <emil.saxlund@live.se>
2020-09-18 11:33:40 +0200

    login profile

--- src/views/Account/index.js
+++ src/views/Account/index.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { loadUser, logout } from '../../store/actions/user'
 
 import AppLogin from '../../components/app-account/app-login'
 import AppRegister from '../../components/app-account/app-register'
+import AppAccountProfile from '../../components/app-account/app-account-profile'
 import AppBrands from '../../components/app-brands'
 
-export default function Account() {
-  return (
-    <div>
-    
-    <div className="container">
+
+const checkLoggedIn = (loggedIn, user, logout) => {
+  if(!loggedIn && user !== null){
+    return (
       <div className="row pt-5">
         <AppLogin/>
         <AppRegister/>
       </div>
+    )
+  } else {
+    return (
+        <AppAccountProfile user={user}/>
+    )
+  }
+
+}
+
+export default function Account() {
+
+  const user = useSelector(state => state.user.user)
+  const loggedIn = useSelector(state => state.user.loggedIn)
+  
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(loadUser())
+  }, [dispatch])
+
+
+  return (
+    <div>
+    
+    <div className="container">
+        {checkLoggedIn(loggedIn, user, () => dispatch(logout()))}
     </div>
     <AppBrands/>
   </div>
