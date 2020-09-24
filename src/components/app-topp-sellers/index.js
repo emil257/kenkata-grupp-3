2607314a5117dd8f82d9485a4bb9a7ecd345706d
Jonas Näsman <jonas.nasman87@gmail.com>
2020-09-24 13:10:54 +0200

    a

--- src/components/app-topp-sellers/index.js
+++ src/components/app-topp-sellers/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './index.css'
 
 import AppTopSellers from "../app-section-headers/top-sellers";
-import Product1 from '../app-product/'
+import Product from '../app-product/'
+import { useSelector } from 'react-redux';
 
 export default function Topsellers({handleOpenModal}) {
+
+  const product1 = useSelector(state => state.products.products[0])
+
+
     return (
       <div>
         <div className="app-top-sellers">
           <div className="container">
             <AppTopSellers />
             <div className="product-container">
-                <Product1 className="product-background main-seller" handleOpenModal={handleOpenModal}/>    
-                <Product1 tag={'new'} className="product-background" handleOpenModal={handleOpenModal}/>    
-                <Product1 tag={'sale'} className="product-background" handleOpenModal={handleOpenModal}/>
+                <Product className="product-background main-seller" handleOpenModal={handleOpenModal}/>    
+                <Product tag={'new'} className="product-background" handleOpenModal={handleOpenModal}/>    
+                <Product tag={'sale'} className="product-background" handleOpenModal={handleOpenModal}/>
             </div>
           </div>
         </div>
