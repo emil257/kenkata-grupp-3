2c77f5d8307b4db15ed38772c8dfa1159003267c
Jonas Näsman <jonas.nasman87@gmail.com>
2020-09-28 10:14:31 +0200

    shipping radio button warning fix

---
+++ src/components/shoppingcart/shopping-cart-step-3/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function OrderComplete() {
+    return (
+        <div>
+            <div>
+                <h1>Order Complete</h1>
+            </div>
+        </div>
+    )
+}
