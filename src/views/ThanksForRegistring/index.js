4a879b5c3590c49a3a0ecddce7d1d992df6aca18
Danial Naseri <naseri.daniel24@gmail.com>
2020-09-30 13:08:47 +0200

    updating latest

--- src/views/ThanksForRegistring/index.js
+++ src/views/ThanksForRegistring/index.js
@@ -3,15 +3,26 @@ import "./index.css"
 
 export default function Index() {
   return (
-    <div className="col-12 col-lg-6">
+    <div className="col-12 col-lg-6 card-margin">
       <div className="order-summary">
         <div className="order-header d-flex align-items-center justify-content-center">
-          <p className="weight-medium text-size-24">CART TOTALS</p>
+          <p className="weight-medium text-size-24"> </p>
         </div>
         <div className="order-content">
-          
-          <button className="btn custom-theme-btn text-size-18 btn-p-t-c mt-3">
-            PLACE ORDER
+
+          <div className="header-justify ">
+            <div className="">
+              <h2 className=" ">
+                THANKS FOR REGISTRING
+              </h2>
+              <p className="">
+                We hope you enjoy your shopping with us!
+              </p>
+            </div>
+          </div>
+
+          <button className="btn custom-theme-btn text-size-18 btn-p-t-c margin-button">
+            LOGIN
           </button>
         </div>
       </div>
