6edf0435162b0f5aac930fbb5c909721aab93ef6
frontendmikeholmstrom <mikael_holmstrom@hotmail.com>
2020-10-02 15:43:35 +0200

    userfix

--- src/components/app-account/app-account-profile-new/order/index.js
+++ src/components/app-account/app-account-profile-new/order/index.js
@@ -11,10 +11,10 @@ export default function Index({ order, number }) {
                     <div className="custom-hr-1"></div>
 
                     <div className="item-top-title-1 font-weight-bold">#</div>
-                    <div className="item-top-title-2 font-weight-bold">Date</div>
-                    <div className="item-top-title-3 font-weight-bold">Status</div>
-                    <div className="item-top-title-4 font-weight-bold">Recipe ID</div>
-                    <div className="item-top-title-5 font-weight-bold">Total amount</div>
+                    <div className="item-top-title-2 font-weight-bold margin-custom">Date</div>
+                    <div className="item-top-title-3 font-weight-bold ">Status</div>
+                    <div className="item-top-title-4 font-weight-bold ">Receipt ID</div>
+                    <div className="item-top-title-5 font-weight-bold ">Total amount</div>
 
                     <div className="custom-hr-2"></div>
 
@@ -29,25 +29,12 @@ export default function Index({ order, number }) {
 
                     {/* Middle Title-Items */}
 
-                    <div className="item-middle-title-1 font-weight-bold">Product id</div>
+                    <div className="item-middle-title-1 font-weight-bold">Product id <span className="d-none product-id-sm">/Price</span> </div>
                     <div className="item-middle-title-2 font-weight-bold">Product name</div>
                     <div className="item-middle-title-3 font-weight-bold">Product Price</div>
 
                     <div className="custom-hr-4"></div>
 
-                    {/* Shows only on small screens */}
-                    <div className="d-none custom-hr-5"></div>
-                    <div className="d-none custom-hr-6"></div>
-                    <div className="d-none custom-hr-7"></div>
-                    <div className="d-none custom-hr-8"></div>
-                    <div className="d-none custom-hr-9"></div>
-                    <div className="d-none custom-hr-10"></div>
-                    <div className="d-none custom-hr-11"></div>
-                    <div className="d-none custom-hr-12"></div>
-                    <div className="d-none custom-hr-13"></div>
-                    <div className="d-none custom-hr-14"></div>
-                    <div className="d-none custom-hr-15"></div>
-                    <div className="d-none custom-hr-16"></div>
 
                     {/* Bottom Order-Items */}
 
